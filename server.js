3f6e847f951e1f7cf61a9a8d401d83afa854ea1f
Rahul Tarak <rahultarak12345@gmail.com>
2017-09-20 06:17:13 +0000

    Messing with headers

--- server.js
+++ server.js
@@ -7,58 +7,85 @@ var mysql = require('mysql');
 var sha512 = require('sha512');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var _ = require('underscore');
 //Server
 var express = require('express');
 var app = express();
 
+app.use(express.logger());
 app.use(bodyParser.json());
+  app.use(allowCrossDomain);
+
+function allowCrossDomain(req, res, next) {
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+  var origin = req.headers.origin;
+  if (_.contains(app.get('allowed_origins'), origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+}
+  
 
 var server = app.listen(port);
-
-    /*  Use for user generation 
-        var key = ""//Salt here;
-        var hasher = sha512.hmac(key);
-        var hash = hasher.finalize("")// Password here;
-        console.log( hash.toString('hex')); */
 var con = mysql.createConnection({
-  host: getenv('IP'),
-  user: getenv('C9_USER'),
-  password: "",
-  database: "c9"
+    host: getenv('IP'),
+    user: getenv('C9_USER'),
+    password: "",
+    database: "c9"
 });
 con.connect(function(err) {
 
-  if (err)  console.log("Error :" + err);
-  else console.log("Successful Db Con");
+    if (err) console.log("Error :" + err);
+    else console.log("Successful Db Con");
 });
 // HTTP Requests
 app.post('/login', function(req, res) {
     console.log(req.body);
-    var loginMessage,loginStatus;
-    // Here is the problem
+    //console.log(req.query);
+    var loginMessage, loginStatus;
     var email = req.body.email;
     var password = req.body.pword;
-    con.query("SELECT Name,Password,Salt FROM Users WHERE Email ='" + email + "';" , function (err, result, fields) {
-        if (err)  console.log("Error :" + err);
+    console.log("SELECT Name,Password,Salt FROM Users WHERE Email ='" + email + "';");
+    con.query("SELECT Name,Password,Salt FROM Users WHERE Email ='" + email + "';", function(err, result, fields) {
+        if (err) console.log("Error :" + err);
+        if(result != ""){
         var key = result[0].Salt;
-        console.log(key);
+        //console.log(key);
         var hasher = sha512.hmac(key);
         var hash = hasher.finalize(password);
         var finalPassword = hash.toString('hex');
-        if(finalPassword == result[0].Password){
+        if (finalPassword == result[0].Password) {
             console.log("Login Succesfull");
             loginMessage = "Login Succesfull";
-            loginStatus = true; 
-        } else {
+            loginStatus = true;
+        }
+        else {
             console.log("Wrong Password");
             loginMessage = "Wrong Password";
             loginStatus = false;
         }
+    } else {
+        loginMessage = "No users Found";
+        loginStatus = false;
+    }
+     
+     
         res.status(201).json({
-            message : loginMessage,
-            status : loginStatus
-            
-        });
+            message: loginMessage,
+            status: loginStatus
+
+        });   
     });
-  
+
 });
+/*  Use for user generation 
+var key = ""//Salt here;
+var hasher = sha512.hmac(key);
+var hash = hasher.finalize("")// Password here;
+console.log( hash.toString('hex')); */
