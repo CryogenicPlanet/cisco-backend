0b3349d9c2c71c2aee9a55f69d3eb4a76c53bac3
Rahul Tarak <rahultarak12345@gmail.com>
2017-10-03 17:09:06 +0000

    Server.js Comments

--- server.js
+++ server.js
@@ -2,53 +2,57 @@
 var port = process.env.PORT;
 
 //Libraries
-var getenv = require('getenv');
-var mysql = require('promise-mysql');
-var sha512 = require('sha512');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var cors = require("cors");
+var getenv = require('getenv'); // Library for Enviroment Variables, Used for Db Conn
+var mysql = require('promise-mysql'); // Mysql Library, With Node Promises
+var sha512 = require('sha512'); // Sha512 Library, Sha512 is a hash
+var bodyParser = require('body-parser'); // Library for parsing data
+var jsonParser = bodyParser.json(); // Using Data type Json
+var cors = require("cors"); // Library for handling access headers
 //Modules
-var user = require("./user.js");
-var borrow = require("./borrow.js")
-//Server
-var express = require('express');
+var user = require("./user.js"); // Object of our module User.js
+var borrow = require("./borrow.js") // Object of our module Borrow.js
+//Server Don't worry about this
+var express = require('express'); // Framework for Node
 var app = express();
 app.use(express.logger());
 app.use(cors());
 app.use(bodyParser.json());
 var server = app.listen(port);
 
-//Db connection
 
-var con = null;
+//DataBase connection using promises
+var con = null; 
 mysql.createConnection({
     host: getenv('IP'),
     user: getenv('C9_USER'),
     password: "",
     database: "c9"
 }).then(function(connection) { con = connection });
-// con.connect(function(err) {
+/* Old redudant Db Connection Code
+ con.connect(function(err) {
 
-//     if (err) console.log("Error :" + err);
-//     else console.log("Successful Db Con");
-// });
-// HTTP Requests
-//POST Request
-app.post('/login', function(req, res) {
-    user.loginUser(req, res, con);
+     if (err) console.log("Error :" + err);
+else console.log("Successful Db Con");
+ }); */
+
+/*HTTP Request Handling
+Handle the request send for data or pages from front-end, browser, user below
+*/
+//Handling Requests of type POST, used to send Data to the server or database
+app.post('/login', function(req, res) { // Request to Log User IN
+    user.loginUser(req, res, con); // Calling function .loginUser() of Object User passing the input, output and database connection
 });
-app.post('/Signup', function(req, res) {
-    user.newUser(req, res, con);
+app.post('/Signup', function(req, res) {// Request to Sign User Up
+    user.newUser(req, res, con); // Calling function .newUser() of Object User passing the input, output and database connection
 });
-app.post('/borrow',function(req, res) {
-    borrow.borrowBooks(req,res,con);
+app.post('/borrow',function(req, res) {// Request to Borrow a Book
+    borrow.borrowBooks(req,res,con); // Calling function .borrowBooks() of Object borrow passing the input, output and database connection
 });
 
-// GET Request
-app.get('/verify', function(req, res) {
-    user.verify(req, res, con);
+// Handling Requests of type GET, used to get Data from the server or databae
+app.get('/verify', function(req, res) { // Request to verify email after signup, This request is from the browser after email link is clicked
+    user.verify(req, res, con); // Calling function .verify() of Object User passing the input, output and database connection
 });
-app.get('/newbooks', function(req, res) {
-    user.followerBooks(req.query.uuid, res, con);
+app.get('/newbooks', function(req, res) { // Request to get a User's follower's New Books to be displayed on the timeline. This request is ajax call from front-end made after succesful login
+    user.followerBooks(req.query.uuid, res, con); // Calling function .followerBooks() of Object User passing the uuid(unique user id), output and database connection
 });
